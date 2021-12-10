74e09e013fd8abb7716594c2f6513f5e1866243c
MqrkD <91572258+MqrkD@users.noreply.github.com>
2021-12-10 17:53:40 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
 const Constraint = Matter.Constraint;
-var bobObject1,bobObject2,bobObject3, bobObject4,bobObject5, roofObject
+var bob1, bob2, bob3, bob4, bob5, roofObject
 var rope1,rope2,rope3, rope4,rope5;
 var world;
 
@@ -21,15 +21,15 @@ function setup() {
 	roofObject=new roof(400,250,230,20);
 	bob1 = new bob(320,575,40);
 	bob2 = new bob(360,575,40);
-	bob3 = new bob(280, 575, 40);
-	bob4 = new bob(360, 575, 40);
-	bob5 = new bob(400, 575, 40);
+	bob3 = new bob(400, 575, 40);
+	bob4 = new bob(440, 575, 40);
+	bob5 = new bob(480, 575, 40);
 	
-	rope3 = new rope(bob3.body, roofObject.body,-120, 0);
-	rope1=new rope(bob1.body, roofObject.body,-80, 0);
-	rope2=new rope(bob2.body, roofObject.body,-40, 0);
-	rope4 = new rope(bob4.body, roofObject.body,00, 0);
-	rope5 = new rope(bob5.body, roofObject.body, 40, 0);
+	rope1=new rope(bob1.body, roofObject.body,	-80, 0);
+	rope2=new rope(bob2.body, roofObject.body,	-40, 0);
+	rope3 = new rope(bob3.body, roofObject.body, 0, 0);
+	rope4 = new rope(bob4.body, roofObject.body, 40, 0);
+	rope5 = new rope(bob5.body, roofObject.body, 80, 0);
 	
 	
 	Engine.run(engine);
@@ -53,15 +53,13 @@ function draw() {
   bob4.display();
   bob5.display();
   
-  
- 
+}
 
 function keyPressed() {
 	if(keyCode === UP_ARROW) {
 
-		Matter.Body.applyForce(bobObject1.body.bobObject1.body.position,
-								{x:350, y:-45})
+		Matter.Body.applyForce(bob1.body, bob1.body.position,
+								{x: -50, y: -45})
 
 	}
-  }
 }
